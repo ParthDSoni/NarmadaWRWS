0654b852a06b7bbfd8e5e2b029cacb7e335da11c
ParthDSoni <105353565+ParthDSoni@users.noreply.github.com>
2025-07-02 17:30:08 +0530

    Add files via upload

---
+++ NWRWS.IService/System/ICouchDBMasterService.cs
@@ -0,0 +1,16 @@
+﻿using NWRWS.Model.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWRWS.IService.System
+{
+    public interface ICouchDBMasterService : IDisposable
+    {
+        CouchDBModel Get();
+
+        bool InsertOrUpdate(CouchDBModel objSMTPModel, out string strErrorMessage);
+    }
+}
---
+++ NWRWS.IService/System/ISMTPMasterService.cs
@@ -0,0 +1,16 @@
+﻿using NWRWS.Model.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWRWS.IService.System
+{
+    public interface ISMTPMasterService
+    {
+        SMTPModel Get();
+        bool InsertOrUpdate(SMTPModel objSMTPModel, out string strErrorMessage);
+        bool UpdateSMTPEnvironment(string strSMTPIsTest, out string strErrorMessage);
+    }
+}
