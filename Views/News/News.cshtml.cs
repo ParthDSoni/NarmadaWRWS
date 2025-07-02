73fc0aee6c231b59d5160799ec5dfad5d44576a3
ParthDSoni <105353565+ParthDSoni@users.noreply.github.com>
2025-07-02 17:45:34 +0530

    Add files via upload

---
+++ Views/News/News.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NWRWS.Webs.Views.News
+{
+    public class NewsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
